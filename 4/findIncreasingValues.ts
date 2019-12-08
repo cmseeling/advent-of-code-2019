3add2dd87c93c9706d4924c816161ee935053ca8
cseeling <cmseeling@gmail.com>
2019-12-07 22:19:08 -0600

    saving day 4 problem 1 progress

--- 4/findIncreasingValues.ts
+++ 4/findIncreasingValues.ts
@@ -1,53 +1,49 @@
 import forEach from 'lodash/forEach';
+import join from 'lodash/join';
 
 const fs = require('fs').promises;
 
-export default async function (rangeStart: number, rangeEnd: number, logFilePath: string = ''): Promise<number[]> {
-  let validNumbers: number[] = [];
+let storageArray: string[] = [];
+
+export default async function (rangeStart: number, rangeEnd: number, logFilePath: string = ''): Promise<string[]> {
+  let startDigit = rangeStart.toString().slice(0, 1);
+  let endDigit = rangeEnd.toString().slice(0, 1);
+
+  findValues(startDigit, endDigit, 0);
+
+  console.log(storageArray);
+  if(logFilePath !== '') {
+    await fs.appendFile(logFilePath, `${join(storageArray, '\n')}`);
+  }
+
+  return storageArray;
+}
+
+const findValues = (startDigit: string, endDigit: string, column: number): string[] => {
+  // console.log(`column: ${column}`);
   const maxLength = 6;
-
-  let currentNumber: number = rangeStart;
-  while(currentNumber <= rangeEnd) {
-    let numberString = currentNumber.toString();
-    let index = 0;
-    let currentDigit: string = numberString[index];
-
-    let newNumberString: string = currentDigit.repeat(6);
-    if(+newNumber > currentNumber) {
-      currentNumber = +newNumber;
+  let array = [];
+  let count = 0;
+  for(count = +startDigit; count <= +endDigit; count++) {
+    // console.log(count);
+    if(column === 5) {
+      array.push(count.toString());
     } else {
-      
+      let tails = findValues(count.toString(), "9", column + 1);
+      forEach(tails, (tail) => {
+        let newString = count.toString() + tail;
+        console.log(newString);
+        if(newString.length === maxLength) {
+          console.log(newString);
+          storageArray.push(newString);
+        } else {
+          array.push(newString);
+        }
+      });
     }
   }
 
-  return validNumbers;
-}
-
-let storageArray: string[] = [];
-
-const func = (startDigit: string, prefix: string) => {
-  const maxLength = 6;
-  // if(padLength === 0) {
-  //   return storageArray;
-  // }
-
-  // let paddedString = startDigit.repeat(padLength);
-  // let count = 0;
-  // for(count = +startDigit; count < 10; count++) {
-  //   let newNumber = paddedString.replaceAt()
-  // }
-
-  if(prefix.length > 5) {
-    return [];
-  }
-
-  let count = 0;
-  for(count = +startDigit; count < 10; count++) {
-    let tails = func(startDigit, prefix + count);
-    forEach(tails, (tail: string) => {
-      storageArray.push(prefix + tail);
-    });
-  }
+  return array;
 }
 
 /*
--- 4/problem1.ts
+++ 4/problem1.ts
@@ -1,13 +1,15 @@
 import forEach from 'lodash/forEach';
+import findIncreasingValues from './findIncreasingValues';
 
 (async () => {
   try {
     console.log('Day 4 Problem 1');
+    const outputFilePath = './4/output.txt';
 
     const start = 402328;
     const end = 864247;
 
-    
+    let increasingNumbers = await findIncreasingValues(start, end, outputFilePath);    
   }
   catch(e) {
     console.log(e);
